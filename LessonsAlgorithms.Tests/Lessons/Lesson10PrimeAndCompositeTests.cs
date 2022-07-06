5ec4d2ff14fe69d61fed9cb2e62f16d551e56bab
Angelo G Tinti <angelo.tinti@gmail.com>
2022-07-05 22:28:54 -0300

    Adding solution for Lesson 10 > CountFactors.

---
+++ LessonsAlgorithms.Tests/Lessons/Lesson10PrimeAndCompositeTests.cs
@@ -0,0 +1,23 @@
+﻿using LessonsAlgorithms.Lessons;
+using Xunit;
+
+namespace LessonsAlgorithms.Tests.Lessons
+{
+    public class Lesson10PrimeAndCompositeTests
+    {
+        [Theory]
+        [InlineData(24, 8)]
+        [InlineData(48, 10)]
+        [InlineData(27043111, 4)]
+        [InlineData(16, 5)]
+        [InlineData(36, 9)]
+        [InlineData(4999696, 45)]
+        [InlineData(39992976, 135)]
+        public void CountFactorsTests(int N, int expectedFactors)
+        {
+            var factors = new Lesson10PrimeAndComposite().CountFactos(N);
+
+            Assert.Equal(expectedFactors, factors);
+        }
+    }
+}
---
+++ LessonsAlgorithms/Lessons/Lesson10PrimeAndComposite.cs
@@ -0,0 +1,24 @@
+﻿namespace LessonsAlgorithms.Lessons
+{
+    public class Lesson10PrimeAndComposite
+    {
+        public int CountFactos(int N)
+        {
+            var factors = 0;
+            var sqrtRoot = Math.Sqrt(N);
+            for (int i = 1; i < sqrtRoot; i++)
+            {
+                if (N % i == 0)
+                {
+                    factors++;
+                }
+            }
+            factors = factors * 2;
+            if (sqrtRoot == Math.Truncate(sqrtRoot))
+            {
+                factors++;
+            }
+            return factors;
+        }
+    }
+}
